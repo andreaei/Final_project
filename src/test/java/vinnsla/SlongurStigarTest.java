83189f8c69055b848e4686dac79bfa4bd668c3ac
Andrea E <ane28@hi.is>
2025-04-15 19:36:14 +0000

    changes due to mvn test not working

---
+++ src/test/java/vinnsla/SlongurStigarTest.java
@@ -0,0 +1,13 @@
+package vinnsla;/******************************************************************************
+ *  Nafn    : Andrea Eiríksdóttir
+ *  T-póstur: ane28@hi.is
+ *
+ *  Lýsing  : Lýsing
+ *
+ *
+ *****************************************************************************/
+public class SlongurStigarTest {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/test/java/vinnsla/TeningurTest.java
@@ -0,0 +1,13 @@
+package vinnsla;/******************************************************************************
+ *  Nafn    : Andrea Eiríksdóttir
+ *  T-póstur: ane28@hi.is
+ *
+ *  Lýsing  : Lýsing
+ *
+ *
+ *****************************************************************************/
+public class TeningurTest {
+    public static void main(String[] args) {
+        
+    }
+}
